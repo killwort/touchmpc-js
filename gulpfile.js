c888deaeff04310b94fbde7f3f5e0079c3085c0d
Andrey Zakharov <killwort@gmail.com>
2016-07-30 22:36:27 +0300

    Styling

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+var gulp=require('gulp');
+var less=require('gulp-less');
+var _concat=require('gulp-concat');
+
+gulp.task('default',function(){
+	return gulp.src('src/*.less')
+		.pipe(less())
+		.pipe(_concat('main.css'))
+		.pipe(gulp.dest('res'));
+});
--- js/nowPlaying.js
+++ js/nowPlaying.js
@@ -2,17 +2,34 @@ const backbone = require('backbone');
 const ipc = require('electron').ipcRenderer;
 const artFetcher=require('../js/artFetcher');
 module.exports = backbone.View.extend({
+    events:{
+        'click .command-repeat':'toggleRepeat',
+        'click .command-shuffle':'toggleShuffle',
+        'click .command-previous':'playPrevious',
+        'click .command-stop':'playStop',
+        'click .command-play':'playPlay',
+        'click .command-next':'playNext'
+    },
+    className:'nowPlaying',
     initialize: function () {
         this.render();
         this.on('mpd-update', this.update);
         this.on('showing', this.show);
         this.on('hiding', this.hide);
+        $(window).on('resize',_.bind(this.fixLayout,this));
         this.playlistModel = new (require('../js/playlistModel'))();
         this.playlistModel.fetch();
         this.artFetchCache = {};
     },
+    fixLayout: function(){
+        var w=$(window).width();
+        var h=$(window).height();
+        this.$el.toggleClass('layout-h',w>h);
+        this.$el.toggleClass('layout-v',w<h);
+    },
     render: function () {
         backbone.$.get('nowPlaying.html').then(data => this.el.innerHTML = data);
+        this.fixLayout();
     },
     show: function () {
         this.update();
@@ -25,6 +42,30 @@ module.exports = backbone.View.extend({
             this.el.className = 'hidden';
             this.trigger('hidden');
         });
+    },
+    toggleRepeat:function(){
+        if(this.$('.command-repeat').is('.active'))
+            mpdCommand('repeat 0');
+        else
+            mpdCommand('repeat 1');
+    },
+    toggleShuffle:function(){
+        if(this.$('.command-shuffler').is('.active'))
+            mpdCommand('random 0');
+        else
+            mpdCommand('random 1');
+    },
+    playNext: function() {
+        mpdCommand('next');
+    },
+    playPrevious: function() {
+        mpdCommand('previous');
+    },
+    playStop: function() {
+        mpdCommand('stop');
+    },
+    playPlay: function() {
+        mpdCommand('play');
     },
     update: function () {
         var that = this;
@@ -57,6 +98,8 @@ module.exports = backbone.View.extend({
                 }
                 if (elem.data('css-property'))
                     elem.css(elem.data('css-property'), value);
+                else if (elem.data('css-class'))
+                    elem.toggleClass(elem.data('css-class'),value==(elem.data('true-value')||1));
                 else if (elem.data('attribute')){
                     if(elem.attr(elem.data('attribute'))!=value)
                         elem.attr(elem.data('attribute'), value);
